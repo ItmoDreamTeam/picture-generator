f3345807404df4a60e185a95fcf2ef9306bc6a11
Kirill <smirnowku@gmail.com>
2017-11-09 01:10:53 +0300

    change app id

--- script/constants.js
+++ script/constants.js
@@ -1,6 +1,6 @@
 const PICTURE_WIDTH = 640;
 const PICTURE_HEIGHT = 480;
 
-const VK_APP_ID = 6248043;
+const VK_APP_ID = 6252305;
 const FS_ROOT_URL = "http://46.101.92.178:8080";
 const PROXY_ROOT_URL = "http://46.101.92.178:8181";
