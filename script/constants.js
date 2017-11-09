8f4d2f4d756c453cfe51d89bc018ea89230f1067
Kirill <smirnowku@gmail.com>
2017-11-09 11:26:37 +0300

    change proxy url

--- script/constants.js
+++ script/constants.js
@@ -3,4 +3,4 @@ const PICTURE_HEIGHT = 480;
 
 const VK_APP_ID = 6252305;
 const FS_ROOT_URL = "http://46.101.92.178:8080";
-const PROXY_ROOT_URL = "http://46.101.92.178:8181";
+const PROXY_ROOT_URL = "https://picture-generator-proxy.herokuapp.com";
